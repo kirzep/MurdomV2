9f4574b9039769ce4ff717579a674575cbcd62bd
Цепелёв Кирилл <kirzep@gmail.com>
2025-06-12 09:17:03 +0300

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,22 @@
 module.exports = {
   apps: [
     {
-      name: 'cat-archive', // Имя вашего приложения
-      script: 'npm',         // Запускаем через npm
-      args: 'start',         // Выполняем команду "npm start"
+      name: 'cat-archive',
+      script: 'npm',
+      args: 'start',
 
       // Переменные окружения для продакшена
       env_production: {
         NODE_ENV: 'production',
-        // Укажите здесь ваш домен или IP-адрес
+        
+        // Эта переменная нужна для корректной работы NextAuth
         NEXTAUTH_URL: 'http://82.202.143.74', 
         
-        // Вставьте сюда ваш секретный ключ из файла .env.local
+        // НОВАЯ ПЕРЕМЕННАЯ: Этот же адрес для доступа к файлам
+        // Префикс NEXT_PUBLIC_ делает ее доступной на клиенте
+        NEXT_PUBLIC_APP_URL: 'http://82.202.143.74',
+
+        // Вставьте сюда ваш секретный ключ
         NEXTAUTH_SECRET: '0f69503ba1641b3d573bf6697df11338',
       },
     },
